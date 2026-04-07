6ffb35b3d8d1e4195e320c2a0de57623f90a841a
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-04-07 15:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const makeChange = (c) => {
-	c = Number(c);
+    c = Number(c);
 
-	let q = Math.floor(c / 25);
-	c = c % 25;
+    let q = Math.floor(c / 25);
+    c = c % 25;
 
-	let d = Math.floor(d / 10);
-	d = d % 10;
+    let d = Math.floor(c / 10);
+    c = c % 10;
 
-	let n = Math.floor(n / 5);
-	n = c % 5
+    let n = Math.floor(c / 5);
+    c = c % 5;
 
-	let p = c
+    let p = c;
 
-	return { q, d, n, p };
+    return { q, d, n, p };
 };
+
+console.log(makeChange(47)); 
+console.log(makeChange(24));
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
